3d70c12e8fc05137dda4c4e3e202c173e730cf49
nurysyvas <daggerok2012@gmail.com>
2018-07-06 20:15:45 +0300

    COMMIT comparison good  + list + revers+ revers + count symbols

--- Main.java
+++ Main.java
@@ -7,7 +7,6 @@ public class Main {
 
     public static void main(String args[]) throws ValueCantbeMinusExseption {
 
-
 //        Scanner reduct = new Scanner(System.in);
 //        String reduction = reduct.nextLine();
 //        System.out.println(reduction);
@@ -16,30 +15,32 @@ public class Main {
 //
 //        reverstext("description");
 
-        ArrayList<IFigure> figures = new ArrayList <IFigure>();
-        IFigure circlee = new Circle(5,5,100);
-        IFigure rectangle = new Rectangle(3, 4);
-        IFigure triangle = new Triangle(4,5,5);
-        figures.add(circlee);
-        figures.add(triangle);
-        figures.add(rectangle);
+        System.out.println(comportext("121"));
+//        System.out.println(countSymbol("abt t"));
+//        ArrayList<IFigure> figures = new ArrayList <IFigure>();
+//        IFigure circlee = new Circle(5,5,100);
+//        IFigure rectangle = new Rectangle(3, 4);
+//        IFigure triangle = new Triangle(4,5,5);
+//        figures.add(circlee);
+//        figures.add(triangle);
+//        figures.add(rectangle);
 
-
-        Rectangle rectanglecom = new Rectangle(1000,100 );
-        System.out.println(rectanglecom.comparison(figures.get(0).S()));
+//
+//        Rectangle rectanglecom = new Rectangle(1000,100 );
+//        System.out.println(rectanglecom.comparison(figures.get(0).S()));
 //        Rectangle rec =new Rectangle(1, 2);
 //        Rectangle rrg =(Rectangle)(figures.get(2));
 //        System.out.println(rrg.comparison(figures.get(1)));
-        Circle[] ar = new Circle[10];
-        float eps = 0.01f;
-        for (int i=0 ; i<ar.length; i++){
-            float x =((float) (( Math.random() * 4)));
-            int y =(int) (Math.random() * 4);
-            int R =(int) (Math.random() * 8);
-            Circle circle = new Circle(x,y,R);
-            ar[i] = circle;
-            ar[i].S();
-        }
+//        Circle[] ar = new Circle[10];
+//        float eps = 0.01f;
+//        for (int i=0 ; i<ar.length; i++){
+//            float x =((float) (( Math.random() * 4)));
+//            int y =(int) (Math.random() * 4);
+//            int R =(int) (Math.random() * 8);
+//            Circle circle = new Circle(x,y,R);
+//            ar[i] = circle;
+//            ar[i].S();
+//        }
 
 //        double k = ((ar[1].getY()-ar[0].getY())/(ar[1].getX()-ar[0].getX()));
 //        double b = ar[0].getY()-k * ar[0].getX();
@@ -58,18 +59,7 @@ public class Main {
 
     }
     public static IFigure creatureFigure (String reduction) throws ValueCantbeMinusExseption {
-//        if (reduction == "cir"){
-//            IFigure firure1 = new Circle(1,2,4);
-//            return  firure1;
-//        }else if(reduction == "rec"){
-//            IFigure rectandle1 = new Rectangle(2,4);
-//            return  rectandle1;
-//        }else if (reduction == "tri"){
-//            IFigure triangle1 = new Triangle(3,5,6);
-//            return triangle1;
-//        } else {
-//            System.out.println("u need enter cir rec tri");
-//        }
+
         switch (reduction){
             case "circ" : IFigure circle1 = new Circle(1,1,3);
             return circle1;
@@ -80,9 +70,29 @@ public class Main {
         }
         return null;
     }
-    public static void reverstext (String text){
+    public static StringBuffer reverstext (String text){
         StringBuffer textt= new StringBuffer(text);
-        System.out.println(textt.reverse());
+        StringBuffer reverText = (textt.reverse());
+        return reverText;
     }
+    public static int comportext (String text){
+        StringBuffer textcomp = new StringBuffer(text);
+        if (textcomp.toString().equals(textcomp.reverse().toString())){
+            return 1;
+        }else {
+            return -1;
+        }
+    }
+
+    public static int countSymbol (String text){
+        int counter = 0;
+        for(int i=0; i<text.length(); i++) {
+            if(text.charAt(i) != ' ') {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 
 }
